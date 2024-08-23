79358260d033fbbf7b5775e12be4bc9a11c78a7a
Mayra Amaral <mayracunha18@gmail.com>
2024-08-23 01:54:39 -0300

    feat: initial structure

---
+++ src/main/java/playground/graphql/GraphqlPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package playground.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlPlaygroundApplication.class, args);
+    }
+
+}
